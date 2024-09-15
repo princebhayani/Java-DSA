78649d32e5cf9c7fc14eeaeb35247ad8a6f9d648
princebhayani <princebhayani192003@gmail.com>
2024-09-15 23:18:46 +0530

    add/update algorithms

---
+++ 41. Graphs part-5/Disjoint_Set_Union.java
@@ -0,0 +1,43 @@
+public class Disjoint_Set_Union {
+    static int n =7; // vertices
+    static int par[] = new int[n];
+    static int rank[] = new int[n];
+
+    public static void init(){
+        for(int i =0;i<n;i++){
+            par[i]=i;
+        }
+    }
+
+    public static int find(int x){
+        if(par[x]==x){
+            return x;
+        }
+        return par[x] = find(par[x]);
+    }
+
+    public static void union(int a, int b){
+        int parA = find(a);
+        int parB = find(b);
+
+        if(rank[parA] == rank[parB]){
+            par[parB] =parA;
+            rank[parA]++;
+        }else if(rank[parA] < rank[parB]){
+            par[parA] =parB;
+        }else{
+            par[parB]= parA;
+        }
+    }
+    public static void main(String[] args) {
+        init();
+        union(1, 3);
+        System.out.println(find(3));
+        union(2, 4);
+        union(3, 6);
+        union(1, 4);
+        System.out.println(find(3));
+        System.out.println(find(4));
+        union(1, 5);
+    }
+}
--- 41. Graphs part-5/Flood_Fill_Algorithm.java
+++ 41. Graphs part-5/Flood_Fill_Algorithm.java
@@ -1,25 +1,32 @@
 public class Flood_Fill_Algorithm {
-    public void helper(int[][] image, int sr, int sc, int color, boolean vis[][], int orgCol){
-        if(sr<0 || sc<0 || sr >= image.length || sc >= image[0].length || vis[sr][sc] || image[sr][sc] != orgCol){
-            return;
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if (image[sr][sc] == color) {
+            return image;
         }
-        image[sc][sr]=color;
-        //left
-        helper(image, sr, sc-1, color, vis, orgCol);
-        //right
-        helper(image, sr, sc+1, color, vis, orgCol);
-        //up
-        helper(image, sr-1, sc, color, vis, orgCol);
-        //down
-        helper(image, sr+1, sc, color, vis, orgCol);
-    }
-
-    public int[][] floodFill(int[][] image, int sr, int sc, int color){
-        boolean vis[][] = new boolean[image.length][image[0].length];
-        helper(image, sr, sc, color, vis, color);
+        helper(image, sr, sc, color, image[sr][sc]);
         return image;
     }
+
+    public void helper(int[][] image, int sr, int sc, int color, int orgCol) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] != orgCol) {
+            return;
+        }
+        image[sr][sc] = color;
+        // left
+        helper(image, sr, sc - 1, color, orgCol);
+        // right
+        helper(image, sr, sc + 1, color, orgCol);
+        // up
+        helper(image, sr - 1, sc, color, orgCol);
+        // down
+        helper(image, sr + 1, sc, color, orgCol);
+    }
+
     public static void main(String[] args) {
-        
+        int[][] image ={{1,1,1},{1,1,0},{1,0,1}};
+        int sr =1;
+        int sc = 1;
+        int color =2;
+
     }
 }
